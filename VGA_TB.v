cf7a603a581c0784ad912510af0e707224e847ea
Hamza Ali <108686725+Hamza-Ali10@users.noreply.github.com>
2024-05-07 22:48:20 +0300

    Update VGA_TB.v

--- VGA_TB.v
+++ VGA_TB.v
@@ -20,19 +20,23 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module VGA_TB(
-
-    );
+module VGA_TB( );
     
     
     reg clk,reset;
+   
+    reg  [2:0] sw;
+  
     wire [2:0] RGB;
+  
     wire hsync,vsync,video_on;
+  
     wire [9:0] pixel_x, pixel_y;
     
     VGA uut(
     .clk(clk),
     .reset(reset),
+    .sw(sw),
     .RGB(RGB),
     .hsync(hsync),
     .vsync(vsync),
@@ -41,9 +45,8 @@ module VGA_TB(
     .video_on(video_on)
     );
 
-    initial#500 $stop;
     
-    localparam T = 8;
+    localparam T = 4;
  
     always
     begin
@@ -58,11 +61,42 @@ module VGA_TB(
        reset = 'b0;
        #4;
        reset = 'b1;
-       
-       end
 
-     initial begin  
-     $monitor("time =%0t ,clk =%b , reset = %b  ",$time , clk , reset);  
-     end
+       // You can modify switch values over time to observe RGB output changes
+       sw = 3'b000; // Set switch to 000
+       #10;        // Wait for a period
+       
+       sw = 3'b001; // Set switch to 001
+       #10;        // Wait for a period
+       
+       sw = 3'b010; // Set switch to 010
+       #10;        // Wait for a period
+
+       sw = 3'b011; // Set switch to 011
+       #10;        // Wait for a period
+
+       sw = 3'b100; // Set switch to 100
+       #10;        // Wait for a period
+
+       sw = 3'b101; // Set switch to 101
+       #10;        // Wait for a period
+
+       sw = 3'b110; // Set switch to 110
+       #10;        // Wait for a period
+       
+       sw = 3'b111; // Set switch to 111
+       #10;        // Wait for a period
     
+       // #100 sw=$random;
+       end
+      
+      
+     initial
+        begin
+     $monitor("time =%0t ,clk =%b , reset = %b , sw = %b ",$time , clk , reset, sw);  
+     end
+       
+       
+       initial #100 $stop;
+ 
 endmodule
